78669303de5f752c70d0202395b0bc71bff4248e
Michael Dorst <m@mdorst.net>
2019-04-14 14:07:31 -0700

    Use local variables in install script

--- install.zsh
+++ install.zsh
@@ -6,6 +6,7 @@ symlink_prompt()
         echo "~/$1 exists. Please remove it and rerun this script if you would like to symlink it."
     else
         echo "Would you like to symlink $1? [y/n]"
+        local link
         read link
         if echo $link | grep -Eqiw 'y|yes'; then
             ln -s "$HOME/.dotfiles/$1" ~
