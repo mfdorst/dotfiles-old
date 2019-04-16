48b3a26b5db126851396f997c4812cb26a6a38b1
Michael Dorst <m@mdorst.net>
2019-04-15 20:18:14 -0700

    Add antigen installer, and chsh to zsh

--- install.zsh
+++ install.zsh
@@ -14,9 +14,25 @@ symlink_prompt()
     fi
 }
 
+if [[ ! -a "$HOME/.antigen.zsh" ]]; then
+    echo "Would you like to install antigen? [y/n]"
+    read install_antigen
+    if echo $install_antigen | grep -Eqiw 'y|yes'; then
+        curl -L git.io/antigen > .antigen.zsh
+    fi
+fi
+
 symlink_prompt '.bashrc'
 
 symlink_prompt '.zshrc'
 
 symlink_prompt '.vimrc'
+
+if [[ $SHELL != '/bin/zsh' ]]; then
+    echo "Your shell is currently set to $SHELL. Would you like to change it to /bin/zsh?"
+    read change_shell
+    if echo $change_shell | grep -Eqiw 'y|yes'; then
+        chsh -s /bin/zsh
+    fi
+fi
 
