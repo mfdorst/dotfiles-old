4530b0cef704b15a224c962a887802eeae5a6047
Michael Dorst <m@mdorst.net>
2020-04-10 18:22:43 -0700

    Track VSCode settings.json

--- install_scripts/backup.rb
+++ install_scripts/backup.rb
@@ -1,6 +1,6 @@
-def backup(file)
-  path = File.join HOME, file
-  backup_path = File.join HOME, "#{file}.backup"
+def backup(file, dir=nil)
+  path = dir == nil ? File.join(HOME, file) : File.join(HOME, dir, file)
+  backup_path = "#{path}.backup"
 
   unless File.exist? backup_path
     File.rename path, backup_path
--- install_scripts/symlink.rb
+++ install_scripts/symlink.rb
@@ -1,10 +1,11 @@
 require "#{File.dirname __FILE__}/backup"
 require "#{File.dirname __FILE__}/user_ask"
 
-def symlink(dir, file)
-  src_path = File.join HOME, '.dotfiles', dir, file
-  src_path_pretty = File.join '~/.dotfiles', dir, file
-  dest_path = File.join HOME, file
+def symlink(source_dir, file, dest_dir=nil)
+  src_path = File.join HOME, '.dotfiles', source_dir, file
+  src_path_pretty = File.join '~/.dotfiles', source_dir, file
+  dest_path = dest_dir == nil ? File.join(HOME, file) : File.join(HOME, dest_dir, file)
+  dest_path_pretty = dest_dir == nil ? File.join('~', file) : File.join('~', dest_dir, file)
 
   if File.symlink? dest_path
     sym_loc = File.readlink dest_path
@@ -20,14 +21,14 @@ def symlink(dir, file)
     File.delete dest_path
   end
   if File.exist? dest_path
-    case backup file
+    case backup file, dest_dir
     when :success
-      puts "Backing up ~/#{file} -> ~/#{file}.backup".green
+      puts "Backing up #{dest_path_pretty} -> #{dest_path_pretty}.backup".green
     when :failure
-      puts "Couldn't back up file - ~/#{file}.backup already exists. Skipping...".red
+      puts "Couldn't back up file - #{dest_path_pretty}.backup already exists. Skipping...".red
       return
     end
   end
-  puts "Symlinking #{file} -> #{src_path_pretty}\n"
+  puts "Symlinking #{dest_path_pretty} -> #{src_path_pretty}\n"
   File.symlink src_path, dest_path
 end
